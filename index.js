b7cd2868b785cbe6cfe59b71140e291cd9c2d1e1
AdviArvi <sbarge00@gmail.com>
2020-02-20 17:08:19 +0530

    modulus

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-// Import stylesheets
-import './style.css';
+
+
+const butts=document.querySelector(".button");
+function sub() {
+    var n1 = parseInt(document.getElementById("num1").value);
+    console.log(n1);
+    var n2 = parseInt(document.getElementById("num2").value);
+    console.log(n2);
+    var oper = document.getElementById("operator").value;
+
+        if(isNaN(n1)||isNaN(n2))
+        {
+          console.error("Enter valid number");
+        }
+
+        else if (oper === "+") {
+          document.getElementById("result").value = n1 + n2;
+          console.log(n1+n2);
+        } else if (oper === "-") {
+          document.getElementById("result").value = n1 - n2;
+          console.log(n1-n2);
+        } else if (oper === "/") {
+
+          if(n2=='0'&& n1=='0'||n2=='0')
+          {
+            alert("Unable to divide");
+          }
+          else
+          {
+          document.getElementById("result").value = n1 / n2;
+          console.log(n1/n2);
+          }
+        } 
+    
+        else if (oper === "*") {
+          document.getElementById("result").value = n1 * n2;
+          console.log(n1*n2);
+        }
+        
+            else if(oper==='%')
+            {
+                modulus(n1,n2);
+            }
+        }
+      
+        function modulus(n1,n2)
+        {            
+            var aa=n1%n2;
+            document.getElementById("ans").innerHTML= `The modulus of ${n1} and ${n2} is ${aa}`;
+            console.log("This is modulus");
+        }
+    
+        
+        butts.addEventListener('click',sub);
+
+    
